095ed4b1c29e01402419b672d0c3b7d8b48e27fd
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-03-25 18:12:20 -0300

    -

--- app/Models/Area.php
+++ app/Models/Area.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Schema\Blueprint;
 
 class Area extends Model
 {
     use HasFactory;
+
+
+    public static function schema(Blueprint $table)
+    {
+        $table->id();
+        $table->string('name');
+        $table->timestamps();
+    }
 }

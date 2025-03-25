072fbaaf38f731650d10fb8209d17bd417ee00be
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-03-25 18:01:34 -0300

    -

---
+++ database/migrations/2025_03_25_205958_create_demands_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('demands', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Requester
+            $table->foreignId('area_id')->constrained()->onDelete('cascade'); // Demand Area
+            $table->text('description');
+            $table->string('status')->default('pending'); // pending, in progress, resolved
+            $table->boolean('requires_councilor')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('demands');
+    }
+};

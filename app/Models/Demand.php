f2c0b5c1bf7b130f32427dc0f211afc8bc9e0eaa
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-03-25 20:04:47 -0300

    -

--- app/Models/Demand.php
+++ app/Models/Demand.php
@@ -16,6 +16,19 @@ class Demand extends Model
         return $this->hasMany(DemandFile::class);
     }
 
+    /**
+     * The "booted" method of the model.
+     */
+    protected static function booted()
+    {
+        static::creating(function ($model) {
+            // Automatically set the user_id to the currently authenticated user
+            if (auth()->check()) {
+                $model->user_id = auth()->id();  // Sets the authenticated user's ID
+            }
+        });
+    }
+
     protected $casts = [
         'area_id' => 'array',
         'files' => 'array',

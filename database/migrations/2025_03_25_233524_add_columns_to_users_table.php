1fe100895afa0242762799a0f912ac8e26b1c488
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-03-25 20:36:50 -0300

    -

---
+++ database/migrations/2025_03_25_233524_add_columns_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('cpf')->nullable(); // roles: admin, assessor, citizen
+            $table->string('address')->nullable(); // roles: admin, assessor, citizen
+            $table->string('zip')->nullable(); // roles: admin, assessor, citizen
+            $table->string('phone')->nullable(); // roles: admin, assessor, citizen
+            $table->string('instagram')->nullable(); // roles: admin, assessor, citizen
+            $table->string('facebook')->nullable(); // roles: admin, assessor, citizen
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('cpf');
+            $table->dropColumn('address');
+            $table->dropColumn('zip');
+            $table->dropColumn('phone');
+            $table->dropColumn('instagram');
+            $table->dropColumn('facebook');
+        });
+    }
+};

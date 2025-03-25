676a2945a96efbfe870cabc30302258e9669573b
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-03-25 19:40:18 -0300

    -

--- app/Models/DemandFile.php
+++ app/Models/DemandFile.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class DemandFile extends Model
 {
     use HasFactory;
+
+    public function demand()
+    {
+        return $this->belongsTo(Demand::class);
+    }
 }

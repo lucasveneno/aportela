131b2770217c7d29beeb3e72a359d08eb0822bdc
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-03-25 18:23:20 -0300

    -

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,12 +2,16 @@
 
 namespace App\Providers;
 
+use App\Models\Demand;
+use App\Policies\DemandPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+
+        Demand::class => DemandPolicy::class,
     ];
 
     public function boot(): void

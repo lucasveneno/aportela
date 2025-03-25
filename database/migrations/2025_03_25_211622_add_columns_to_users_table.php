37d6f9d4a432e530a0dbd2b3080520de6fd7f147
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-03-25 19:01:38 -0300

    -

--- database/migrations/2025_03_25_211622_add_columns_to_users_table.php
+++ database/migrations/2025_03_25_211622_add_columns_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('region_id')->nullable();
+            $table->json('region_id')->nullable();
             $table->string('role')->default('user'); // roles: admin, assessor, citizen
         });
     }

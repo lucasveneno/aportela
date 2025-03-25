8cdf444c3f21bd677669f49993499f9b6b908a0b
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-03-25 19:38:40 -0300

    -

--- app/Models/Demand.php
+++ app/Models/Demand.php
@@ -8,11 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Demand extends Model
 {
     use HasFactory;
+    
+    protected $table = 'demand_files'; // Ensure this points to the correct table
+
 
     public function demandFiles()
     {
         return $this->hasMany(DemandFile::class);
     }
+
+    
 
     protected $casts = [
         'area_id' => 'array',

125f49709fd5b80f63066efe0b0e24f2ba95ac8b
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-03-26 01:37:03 -0300

    -

--- database/migrations/2025_03_26_003403_add_columns_to_demands_table.php
+++ database/migrations/2025_03_26_003403_add_columns_to_demands_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::table('demands', function (Blueprint $table) {
             $table->string('city')->nullable(); // roles: admin, assessor, citizen
+            $table->string('state')->nullable(); // roles: admin, assessor, citizen
             $table->string('zip')->nullable(); // roles: admin, assessor, citizen
             $table->string('address')->nullable(); // roles: admin, assessor, citizen
             $table->string('latitude')->nullable(); // roles: admin, assessor, citizen
@@ -27,6 +28,7 @@ return new class extends Migration
     {
         Schema::table('demands', function (Blueprint $table) {
             $table->dropColumn('city');
+            $table->dropColumn('state');
             $table->dropColumn('zip');
             $table->dropColumn('address');
             $table->dropColumn('latitude');

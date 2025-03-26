825827c5a87cd19667184f52dbc7ea36284b3b6b
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-03-26 17:27:51 -0300

    -

--- app/Models/Demand.php
+++ app/Models/Demand.php
@@ -43,7 +43,7 @@ class Demand extends Model
     protected $casts = [
         'area_id' => 'array',
         'files' => 'array',
-        'location' > 'array',
+        //'location' > 'array',
     ];
 
 

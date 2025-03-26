eab379d915bfc896ec8edef94e6a9da22d9938e6
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-03-26 13:32:54 -0300

    -

--- app/Models/Demand.php
+++ app/Models/Demand.php
@@ -19,95 +19,44 @@ class Demand extends Model
 
      protected $fillable = [
         'user_id',
-        'city',
-        'state',
-        'zip',
-        'address',
-        'location',
-        'formatted_address',
-    ];
-
-    protected $appends = [
         'location',
     ];
-
-    /**
+/**
+     * The following code was generated for use with Filament Google Maps
      *
-     * The 'latitude' and 'longitude' attributes should exist as fields in your table schema,
-     * holding standard decimal latitude and longitude coordinates.
-     *
-     * The 'location' attribute should NOT exist in your table schema, rather it is a computed attribute,
-     * which you will use as the field name for your Filament Google Maps form fields and table columns.
-     *
-     * You may of course strip all comments, if you don't feel verbose.
+     * php artisan fgm:model-code Demand --lat=latitude --lng=longitude --location=location --terse
      */
 
-    /**
-    * Returns the 'latitude' and 'longitude' attributes as the computed 'location' attribute,
-    * as a standard Google Maps style Point array with 'lat' and 'lng' attributes.
-    *
-    * Used by the Filament Google Maps package.
-    *
-    * Requires the 'location' attribute be included in this model's $fillable array.
-    *
-    * @return array
-    */
-
-    public function getLocationAttribute(): array
-    {
-        return [
-            "lat" => (float)$this->latitude,
-            "lng" => (float)$this->longitude,
-        ];
-    }
-
-    /**
-    * Takes a Google style Point array of 'lat' and 'lng' values and assigns them to the
-    * 'latitude' and 'longitude' attributes on this model.
-    *
-    * Used by the Filament Google Maps package.
-    *
-    * Requires the 'location' attribute be included in this model's $fillable array.
-    *
-    * @param ?array $location
-    * @return void
-    */
-    public function setLocationAttribute(?array $location): void
-    {
-        if (is_array($location))
-        {
-            $this->attributes['latitude'] = $location['lat'];
-            $this->attributes['longitude'] = $location['lng'];
-            unset($this->attributes['location']);
-        }
-    }
-
-    /**
-     * Get the lat and lng attribute/field names used on this table
-     *
-     * Used by the Filament Google Maps package.
-     *
-     * @return string[]
-     */
-    public static function getLatLngAttributes(): array
-    {
-        return [
-            'lat' => 'latitude',
-            'lng' => 'longitude',
-        ];
-    }
-
-   /**
-    * Get the name of the computed location attribute
-    *
-    * Used by the Filament Google Maps package.
-    *
-    * @return string
-    */
-    public static function getComputedLocation(): string
-    {
-        return 'location';
-    }
+     public function getLocationAttribute(): array
+     {
+         return [
+             "lat" => (float)$this->latitude,
+             "lng" => (float)$this->longitude,
+         ];
+     }
+ 
+     public function setLocationAttribute(?array $location): void
+     {
+         if (is_array($location))
+         {
+             $this->attributes['latitude'] = $location['lat'];
+             $this->attributes['longitude'] = $location['lng'];
+             unset($this->attributes['location']);
+         }
+     }
+ 
+     public static function getLatLngAttributes(): array
+     {
+         return [
+             'lat' => 'latitude',
+             'lng' => 'longitude',
+         ];
+     }
+ 
+     public static function getComputedLocation(): string
+     {
+         return 'location';
+     }
 
 
     /**

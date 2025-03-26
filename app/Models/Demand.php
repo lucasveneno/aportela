0dfd312d45de9af6dc0678093deb328e83b1dfd3
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-03-26 17:22:57 -0300

    -

--- app/Models/Demand.php
+++ app/Models/Demand.php
@@ -17,9 +17,8 @@ class Demand extends Model
      */
 
 
-    protected $fillable = [
+     protected $fillable = [
         'user_id',
-        'location',
     ];
 
     protected $appends = [
@@ -43,7 +42,6 @@ class Demand extends Model
     protected $casts = [
         'area_id' => 'array',
         'files' => 'array',
-        //'full_address' => 'array',
     ];
 
 
@@ -60,15 +58,15 @@ class Demand extends Model
      */
 
     /**
-     * Returns the 'latitude' and 'longitude' attributes as the computed 'location' attribute,
-     * as a standard Google Maps style Point array with 'lat' and 'lng' attributes.
-     *
-     * Used by the Filament Google Maps package.
-     *
-     * Requires the 'location' attribute be included in this model's $fillable array.
-     *
-     * @return array
-     */
+    * Returns the 'latitude' and 'longitude' attributes as the computed 'location' attribute,
+    * as a standard Google Maps style Point array with 'lat' and 'lng' attributes.
+    *
+    * Used by the Filament Google Maps package.
+    *
+    * Requires the 'location' attribute be included in this model's $fillable array.
+    *
+    * @return array
+    */
 
     public function getLocationAttribute(): array
     {
@@ -79,19 +77,20 @@ class Demand extends Model
     }
 
     /**
-     * Takes a Google style Point array of 'lat' and 'lng' values and assigns them to the
-     * 'latitude' and 'longitude' attributes on this model.
-     *
-     * Used by the Filament Google Maps package.
-     *
-     * Requires the 'location' attribute be included in this model's $fillable array.
-     *
-     * @param ?array $location
-     * @return void
-     */
+    * Takes a Google style Point array of 'lat' and 'lng' values and assigns them to the
+    * 'latitude' and 'longitude' attributes on this model.
+    *
+    * Used by the Filament Google Maps package.
+    *
+    * Requires the 'location' attribute be included in this model's $fillable array.
+    *
+    * @param ?array $location
+    * @return void
+    */
     public function setLocationAttribute(?array $location): void
     {
-        if (is_array($location)) {
+        if (is_array($location))
+        {
             $this->attributes['latitude'] = $location['lat'];
             $this->attributes['longitude'] = $location['lng'];
             unset($this->attributes['location']);
@@ -113,13 +112,13 @@ class Demand extends Model
         ];
     }
 
-    /**
-     * Get the name of the computed location attribute
-     *
-     * Used by the Filament Google Maps package.
-     *
-     * @return string
-     */
+   /**
+    * Get the name of the computed location attribute
+    *
+    * Used by the Filament Google Maps package.
+    *
+    * @return string
+    */
     public static function getComputedLocation(): string
     {
         return 'location';

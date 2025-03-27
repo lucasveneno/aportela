679bec3602587df1620fab796480213bf76e588d
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-03-27 11:37:54 -0300

    -

---
+++ resources/lang/pt_BR/resources.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Resources Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'areas' => 'Áreas',
+    'categories' => 'Categorias',
+    'demands' => 'Demandas',
+    'regions' => 'Regiões',
+    'users' => 'Usuários',
+
+];

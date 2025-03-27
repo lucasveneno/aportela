6d3c9a7b469db9d650763f2501644b8d733a17b1
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-03-27 15:22:47 -0300

    -

--- app/Filament/Resources/DemandResource.php
+++ app/Filament/Resources/DemandResource.php
@@ -131,19 +131,20 @@ class DemandResource extends Resource
                             Hidden::make('prioridade')
                                 ->default(__('resources.demands.low')),
 
-                            TextInput::make('prioridade')
-                                ->label('Nível de Prioridade')
-                                ->disabled()
-                                ->dehydrated()
-                                ->columnSpan(1),
 
-                            Placeholder::make('descricao_prioridade')
-                                ->label('Justificativa')
-                                ->content(fn($get) => self::descricaoPrioridade($get('criterios') ?? []))
-                                ->columnSpan(2),
 
                         ])->columns(1),
+                        
+                        TextInput::make('prioridade')
+                            ->label('Nível de Prioridade')
+                            ->disabled()
+                            ->dehydrated()
+                            ->columnSpan(1),
 
+                        Placeholder::make('descricao_prioridade')
+                            ->label('Justificativa')
+                            ->content(fn($get) => self::descricaoPrioridade($get('criterios') ?? []))
+                            ->columnSpan(2),
 
 
 

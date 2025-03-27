1a5eb4ed8f85a480d9d9cc485dcd645933bbdf06
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-03-26 22:35:39 -0300

    -

--- app/Filament/Resources/DemandResource.php
+++ app/Filament/Resources/DemandResource.php
@@ -54,16 +54,14 @@ class DemandResource extends Resource
                 Textarea::make('description')->required(),
                 Toggle::make('requires_councilor'),
 
-                Section::make('Priority')
+                Section::make('Critérios para Definição de Prioridades:')
                     ->description('Prevent abuse by limiting the number of requests per period')
                     ->columns(2)
                     ->schema([
                         Placeholder::make('Critérios para Definição de Prioridades:')
                             ->content('✔ Impacto na população (saúde, segurança, mobilidade).'),
-                        Placeholder::make('Critérios para Definição de Prioridades:')
-                            ->content('✔ Impacto na população (saúde, segurança, mobilidade).'),
-                        Placeholder::make('Critérios para Definição de Prioridades:')
-                            ->content('✔ Impacto na população (saúde, segurança, mobilidade).'),
+                        Placeholder::make('')->content('✔ Impacto na população (saúde, segurança, mobilidade).'),
+                        Placeholder::make('')->content('✔ Impacto na população (saúde, segurança, mobilidade).'),
                         Radio::make('priority')
                             ->options([
                                 'max' => 'Prioridade Máxima ',

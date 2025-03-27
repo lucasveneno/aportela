125d18da5d64fc46c8eb5cc7ddb916f8526b965e
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-03-27 18:38:58 -0300

    -

--- app/Filament/Resources/DemandResource.php
+++ app/Filament/Resources/DemandResource.php
@@ -105,12 +105,14 @@ class DemandResource extends Resource
                         ])
                         ->label(__('resources.demands.description'))->required(),
                 ])->columns(1),
+                
                 ToggleButtons::make('applicant_demand_origin')
                     ->label(__('resources.demands.applicant_demand_origin'))
                     ->boolean()
                     ->default(1)
                     ->inline()
                     ->live(), // Makes it update in real-time
+                    
                 Section::make([
 
 

b37c874d0f0e3597a27693186c87fb48655c3d46
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-03-28 15:45:10 -0300

    -

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -16,10 +16,12 @@
             name="application-name"
             content="{{ config('app.name') }}"
         />
+        
         <meta
             name="csrf-token"
             content="{{ csrf_token() }}"
         />
+
         <meta
             name="viewport"
             content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"

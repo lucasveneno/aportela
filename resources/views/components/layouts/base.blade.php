965cfee48745f02a04d2e40b1156960b22f85b70
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-03-28 16:12:39 -0300

    -

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -24,7 +24,7 @@
 
         <meta
             name="viewport"
-            content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"
+            content="width=device-width, initial-scale=1"
         />
 
         <x-seo::meta />

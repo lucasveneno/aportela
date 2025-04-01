e90ecd65c3f584dd784087db47c1c2ed2d4c0f8c
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-04-01 10:40:05 -0300

    -

---
+++ database/migrations/2025_04_01_093814_add_columns_to_demands_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('demands', function (Blueprint $table) {
+            $table->string('demand_code')->nullable(); // roles: admin, assessor, citizen
+            $table->string('category_id')->nullable(); // roles: admin, assessor, citizen
+            $table->string('applicant_demand_origin')->nullable(); // roles: admin, assessor, citizen
+            $table->json('applicants')->nullable(); // roles: admin, assessor, citizen
+            $table->string('prioritization_criteria')->nullable(); // roles: admin, assessor, citizen
+            $table->string('priority')->nullable(); // roles: admin, assessor, citizen
+            $table->string('priority_description')->nullable(); // roles: admin, assessor, citizen
+            $table->string('draft')->nullable(); // roles: admin, assessor, citizen
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('demands', function (Blueprint $table) {
+            $table->dropColumn('demand_code');
+            $table->dropColumn('category_id');
+            $table->dropColumn('applicant_demand_origin');
+            $table->dropColumn('applicants');
+            $table->dropColumn('prioritization_criteria');
+            $table->dropColumn('priority');
+            $table->dropColumn('priority_description');
+            $table->dropColumn('drafts');
+
+        });
+    }
+};

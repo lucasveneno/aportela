fb08de76f4a045736f805fb9485508bc00ee1473
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-04-03 12:21:54 -0300

    -

--- app/Models/Demand.php
+++ app/Models/Demand.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Demand extends Model
 {
@@ -127,5 +128,10 @@ class Demand extends Model
     public static function getComputedLocation(): string
     {
         return 'location';
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }

27fdc91bccd7066df994753237f60c697680b7bd
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-04-03 11:15:15 -0300

    -

---
+++ app/Filament/Resources/DemandResource/Widgets/DemandsChart.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\Resources\DemandResource\Widgets;
+
+use Filament\Widgets\ChartWidget;
+
+class DemandsChart extends ChartWidget
+{
+    protected static ?string $heading = 'Chart';
+
+    protected function getData(): array
+    {
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Demands created',
+                    'data' => [0, 10, 5, 2, 21, 32, 45, 74, 65, 45, 77, 89],
+                ],
+            ],
+            'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}

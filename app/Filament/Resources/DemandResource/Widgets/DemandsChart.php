c1ba50c165574857a45be3b78af21a8d2c2ca954
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-04-03 11:35:51 -0300

    -

--- app/Filament/Resources/DemandResource/Widgets/DemandsChart.php
+++ app/Filament/Resources/DemandResource/Widgets/DemandsChart.php
@@ -29,7 +29,7 @@ class DemandsChart extends ChartWidget
 
     protected function getData(): array
     {
-        $filters = $this->getFilters();
+        $filters = $this->filters;
 
         $data = Trend::model(Demand::class)
             ->between(

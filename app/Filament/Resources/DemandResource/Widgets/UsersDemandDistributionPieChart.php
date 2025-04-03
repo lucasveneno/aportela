b4cabaf08f97b348984157be4f3ac3cc8a28dd1d
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-04-03 13:02:56 -0300

    -

--- app/Filament/Resources/DemandResource/Widgets/UsersDemandDistributionPieChart.php
+++ app/Filament/Resources/DemandResource/Widgets/UsersDemandDistributionPieChart.php
@@ -140,7 +140,7 @@ class UsersDemandDistributionPieChart  extends ChartWidget
                     ]
                 ],
             ],
-            'cutout' => '60%',
+            'cutout' => '0%',
         ];
     }
 

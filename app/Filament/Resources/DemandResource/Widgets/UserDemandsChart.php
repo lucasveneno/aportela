f386cf74f6863c76bdf5cbdf5b70cbb98ad50bbb
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-04-03 12:14:51 -0300

    -

--- app/Filament/Resources/DemandResource/Widgets/UserDemandsChart.php
+++ app/Filament/Resources/DemandResource/Widgets/UserDemandsChart.php
@@ -2,11 +2,25 @@
 
 namespace App\Filament\Resources\DemandResource\Widgets;
 
+use App\Models\Demand;
+use App\Models\User;
 use Filament\Widgets\ChartWidget;
+use Illuminate\Support\Facades\Auth;
 
 class UserDemandsChart extends ChartWidget
 {
-    protected static ?string $heading = 'Chart';
+    protected static ?string $heading = 'Demand Distribution by User';
+    protected static ?string $maxHeight = '400px';
+    protected static ?string $pollingInterval = null;
+    protected int|string|array $columnSpan = 'md';
+
+    /**
+     * Determine if the widget should be visible
+     */
+    public static function canView(): bool
+    {
+        return Auth::user()->isAdmin();
+    }
 
     protected function getData(): array
     {

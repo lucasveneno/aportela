2b3dae4afc6160a3ab4e6432de76f8789b877f3b
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-04-03 12:05:45 -0300

    -

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -41,7 +41,7 @@ class AppPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
+                //Widgets\AccountWidget::class,
                 DemandsChart::class,
                 //Widgets\FilamentInfoWidget::class,
             ])

37f14e5a0635815a9d5241251072160bbb1075f6
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-04-03 10:50:46 -0300

    -

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,7 +24,7 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function isAdmin(): bool
     {
-        return $this->is_admin === true;
+        return $this->is_admin == true;
     }
 
     public function getRole(): string

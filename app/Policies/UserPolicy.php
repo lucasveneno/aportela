f59a91dda94d2a4351310420690e63f3e534e27f
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-04-03 10:32:02 -0300

    -

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -12,7 +12,7 @@ class UserPolicy
      */
     public function viewAny(User $user): bool
     {
-        return $user->is_admin === '1' && in_array($user->role, ['user']);
+        return $user->is_admin && in_array($user->role, ['user']);
     }
 
     /**
@@ -20,7 +20,7 @@ class UserPolicy
      */
     public function view(User $user, User $model): bool
     {
-        return $user->is_admin === '1' && in_array($user->role, ['user']);
+        return $user->is_admin && in_array($user->role, ['user']);
     }
 
     /**
@@ -28,7 +28,7 @@ class UserPolicy
      */
     public function create(User $user): bool
     {
-        return $user->is_admin === '1' && in_array($user->role, ['user']);
+        return $user->is_admin && in_array($user->role, ['user']);
     }
 
     /**
@@ -36,7 +36,7 @@ class UserPolicy
      */
     public function update(User $user, User $model): bool
     {
-        return $user->is_admin === '1' && in_array($user->role, ['user']);
+        return $user->is_admin && in_array($user->role, ['user']);
     }
 
     /**
@@ -44,7 +44,7 @@ class UserPolicy
      */
     public function delete(User $user, User $model): bool
     {
-        return $user->is_admin === '1' && in_array($user->role, ['user']);
+        return $user->is_admin && in_array($user->role, ['user']);
     }
 
     /**
@@ -52,7 +52,7 @@ class UserPolicy
      */
     public function restore(User $user, User $model): bool
     {
-        return $user->is_admin === '1' && in_array($user->role, ['user']);
+        return $user->is_admin && in_array($user->role, ['user']);
     }
 
     /**
@@ -60,6 +60,6 @@ class UserPolicy
      */
     public function forceDelete(User $user, User $model): bool
     {
-        return $user->is_admin === '1' && in_array($user->role, ['user']);
+        return $user->is_admin && in_array($user->role, ['user']);
     }
 }

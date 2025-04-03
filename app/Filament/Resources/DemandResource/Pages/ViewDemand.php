e6f6502b5b37eae3a7b426b4f191c66d968ce447
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-04-03 16:15:30 -0300

    -

--- app/Filament/Resources/DemandResource/Pages/ViewDemand.php
+++ app/Filament/Resources/DemandResource/Pages/ViewDemand.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\DemandResource\Pages;
 
 use App\Filament\Resources\DemandResource;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
 
@@ -21,8 +22,31 @@ class ViewDemand extends ViewRecord
                 ->action(function () {
                     $this->dispatch('print-page');
                 }),
+            Actions\Action::make('pdf')
+                ->label('Download PDF')
+                ->icon('heroicon-o-document-arrow-down')
+                ->color('success')
+                ->action(function () {
+                    return $this->generatePDF();
+                }),
             Actions\EditAction::make(), // Keep existing edit action
         ];
+    }
+
+    protected function generatePDF()
+    {
+        $pdf = Pdf::loadHTML(
+            view('filament.pdf.demand', [
+                'record' => $this->record,
+                'title' => $this->record->title,
+                'date' => now()->format('Y-m-d'),
+            ])
+        );
+
+        return response()->streamDownload(
+            fn() => print($pdf->output()),
+            "demand-{$this->record->id}.pdf"
+        );
     }
 
     // Optional: Customize the view to add print-specific styling

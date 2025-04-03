cb20ecf7a7c492d4e033e7f0b6fefdda9d36f5b7
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-04-03 16:26:32 -0300

    -

--- app/Filament/Resources/DemandResource/Pages/ViewDemand.php
+++ app/Filament/Resources/DemandResource/Pages/ViewDemand.php
@@ -45,7 +45,7 @@ class ViewDemand extends ViewRecord
 
         return response()->streamDownload(
             fn() => print($pdf->output()),
-            "demand-{$this->record->id}.pdf"
+            "demand-{$this->record->demand_code}.pdf"
         );
     }
 

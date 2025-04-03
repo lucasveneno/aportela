d07e5f268f7549f92a162690be362758e828ad69
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-04-03 16:42:58 -0300

    -

--- resources/views/filament/pdf/demand.blade.php
+++ resources/views/filament/pdf/demand.blade.php
@@ -1,13 +1,47 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-BR">
 
 <head>
+    <meta charset="UTF-8">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>{{ $title }}</title>
     <style>
         body {
             font-family: DejaVu Sans, sans-serif;
-            line-height: 1.6
+            line-height: 1.6;
+            margin: 20px;
+        }
+
+        body {}
+
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            background-color: #f9f9f9;
+        }
+
+        h2 {
+            text-align: center;
+        }
+
+        button {
+            margin-top: 15px;
+            padding: 10px;
+            width: 100%;
+            background-color: #007bff;
+            color: white;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #0056b3;
         }
 
         .header {
@@ -45,11 +79,23 @@
     </div>
 
     <div class="content">
-        {!! $record->description !!}
+
+
+        <div class="container" id="content">
+            <h2>Solicitação de Demanda</h2>
+            <p><strong>Unidade/Setor:</strong> Infraestrutura</p>
+            <p><strong>Responsável:</strong> João da Silva</p>
+            <p><strong>Justificativa:</strong> Necessidade de reparo em via pública devido a buracos.</p>
+            <p><strong>Data do Evento:</strong> 10/04/2025</p>
+            <p>
+                <strong>Especificação:</strong>
+                <br /> {!! $record->description !!}
+            </p>
+        </div>
     </div>
 
     <div class="footer">
-       
+
     </div>
 </body>
 

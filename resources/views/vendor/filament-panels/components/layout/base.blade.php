35c83f0810b20c0591cd8b5f2b399f003aec37e8
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-04-08 10:39:43 -0300

    -

--- resources/views/vendor/filament-panels/components/layout/base.blade.php
+++ resources/views/vendor/filament-panels/components/layout/base.blade.php
@@ -16,7 +16,7 @@
 
         <meta charset="utf-8" />
         <meta name="csrf-token" content="{{ csrf_token() }}" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
 
 
         @if ($favicon = filament()->getFavicon())

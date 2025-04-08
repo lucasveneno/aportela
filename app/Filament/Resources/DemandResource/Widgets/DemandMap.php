cbecc2d1e0a9264f7748cc626485351433affa6f
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-04-08 13:02:00 -0300

    -

--- app/Filament/Resources/DemandResource/Widgets/DemandMap.php
+++ app/Filament/Resources/DemandResource/Widgets/DemandMap.php
@@ -2,112 +2,65 @@
 
 namespace App\Filament\Resources\DemandResource\Widgets;
 
-use App\Filament\Resources\DemandResource\Pages\EditDemand;
-use App\Filament\Resources\DemandResource\Pages\ViewDemand;
-use App\Models\Area;
-use App\Models\Demand;
-use Cheesegrits\FilamentGoogleMaps\Actions\GoToAction;
-use Cheesegrits\FilamentGoogleMaps\Actions\RadiusAction;
-use Cheesegrits\FilamentGoogleMaps\Filters\RadiusFilter;
-use Cheesegrits\FilamentGoogleMaps\Widgets\MapTableWidget;
-use Cheesegrits\FilamentGoogleMaps\Columns\MapColumn;
-use Cheesegrits\FilamentGoogleMaps\Filters\MapIsFilter;
-use Filament\Tables;
-use Filament\Tables\Columns\TextColumn;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\HtmlString;
+use Cheesegrits\FilamentGoogleMaps\Widgets\MapWidget;
 
-class DemandMap extends MapTableWidget
+class DemandMap extends MapWidget
 {
-	protected static ?string $heading = 'Demand Map';
+    protected static ?string $heading = 'Map';
 
-	protected static ?int $sort = 1;
+    protected static ?int $sort = 1;
 
-	protected static ?string $pollingInterval = null;
+    protected static ?string $pollingInterval = null;
 
-	protected static ?bool $clustering = true;
+    protected static ?bool $clustering = true;
 
-	protected static ?string $mapId = 'incidents';
+    protected static ?bool $fitToBounds = true;
 
-	protected int|string|array $columnSpan = 'full';
+    protected static ?int $zoom = 12;
 
+    protected function getData(): array
+    {
+    	/**
+    	 * You can use whatever query you want here, as long as it produces a set of records with your
+    	 * lat and lng fields in them.
+    	 */
+        $locations = \App\Models\Demand::all()->limit(500);
 
-	protected function getTableQuery(): Builder
-	{
-		$query = Demand::query();
-		if (!auth()->user()->isAdmin()) {
-			$query->where('user_id', auth()->id());
-		}
+        $data = [];
 
-		return $query->latest();
+        foreach ($locations as $location)
+        {
+			/**
+			 * Each element in the returned data must be an array
+			 * containing a 'location' array of 'lat' and 'lng',
+			 * and a 'label' string (optional but recommended by Google
+			 * for accessibility.
+			 *
+			 * You should also include an 'id' attribute for internal use by this plugin
+			 */
+            $data[] = [
+                'location'  => [
+                    'lat' => $location->latitude ? round(floatval($location->latitude), static::$precision) : 0,
+                    'lng' => $location->longitude ? round(floatval($location->longitude), static::$precision) : 0,
+                ],
 
-		//return \App\Models\Demand::query()->latest();
-	}
+                'label'     => $location->latitude . ',' . $location->longitude,
+                
+                'id' => $location->getKey(),
 
-	protected function getTableColumns(): array
-	{
-		return [
-
-			//TextColumn::make('latitude'),
-			//TextColumn::make('longitude'),
-			MapColumn::make('location')
-				->extraImgAttributes(
-					fn($record): array => ['title' => $record->latitude . ',' . $record->longitude]
-				)
-				->height('150')
-				->width('150')
-				->type('hybrid')
-				->zoom(16),
-			TextColumn::make('demand_code'),
-			TextColumn::make('area_id')
-				->label(__('resources.categories.area'))
-				->formatStateUsing(fn($state): string => Area::find($state)?->name ?? 'N/A')
-				->sortable(),
-		];
-	}
-
-	protected function getTableFilters(): array
-	{
-		return [
-			RadiusFilter::make('location')
-				->section('Radius Filter')
-				->selectUnit(),
-			MapIsFilter::make('map'),
-		];
-	}
-
-	protected function getTableActions(): array
-	{
-		return [
-			//Tables\Actions\ViewAction::make(),
-			//Tables\Actions\EditAction::make(),
-			Tables\Actions\ViewAction::make()
-				->url(fn($record) => ViewDemand::getUrl(['record' => $record])),
-			Tables\Actions\EditAction::make()
-				->url(fn($record) => EditDemand::getUrl(['record' => $record])),
-			GoToAction::make()
-				->zoom(17)
-				->label('Ver no mapa'),
-			//RadiusAction::make(),
-		];
-	}
-
-	protected function getData(): array
-	{
-		$locations = $this->getRecords();
-
-		$data = [];
-
-		foreach ($locations as $location) {
-			$data[] = [
-				'location' => [
-					'lat' => $location->latitude ? round(floatval($location->latitude), static::$precision) : 0,
-					'lng' => $location->longitude ? round(floatval($location->longitude), static::$precision) : 0,
+				/**
+				 * Optionally you can provide custom icons for the map markers,
+				 * either as scalable SVG's, or PNG, which doesn't support scaling.
+				 * If you don't provide icons, the map will use the standard Google marker pin.
+				 */
+				'icon' => [
+					'url' => url('images/dealership.svg'),
+					'type' => 'svg',
+					'scale' => [35,35],
 				],
-				'id'      => $location->id,
-			];
-		}
+            ];
+        }
 
-		return $data;
-	}
+        return $data;
+    }
 }
---
+++ app/Filament/Resources/DemandResource/Widgets/DemandTableMap.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Filament\Resources\DemandResource\Widgets;
+
+use App\Filament\Resources\DemandResource\Pages\EditDemand;
+use App\Filament\Resources\DemandResource\Pages\ViewDemand;
+use App\Models\Area;
+use App\Models\Demand;
+use Cheesegrits\FilamentGoogleMaps\Actions\GoToAction;
+use Cheesegrits\FilamentGoogleMaps\Actions\RadiusAction;
+use Cheesegrits\FilamentGoogleMaps\Filters\RadiusFilter;
+use Cheesegrits\FilamentGoogleMaps\Widgets\MapTableWidget;
+use Cheesegrits\FilamentGoogleMaps\Columns\MapColumn;
+use Cheesegrits\FilamentGoogleMaps\Filters\MapIsFilter;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\HtmlString;
+
+class DemandTableMap extends MapTableWidget
+{
+	protected static ?string $heading = 'Demand Map';
+
+	protected static ?int $sort = 1;
+
+	protected static ?string $pollingInterval = null;
+
+	protected static ?bool $clustering = true;
+
+	protected static ?string $mapId = 'incidents';
+
+	protected int|string|array $columnSpan = 'full';
+
+
+	protected function getTableQuery(): Builder
+	{
+		$query = Demand::query();
+		if (!auth()->user()->isAdmin()) {
+			$query->where('user_id', auth()->id());
+		}
+
+		return $query->latest();
+
+		//return \App\Models\Demand::query()->latest();
+	}
+
+	protected function getTableColumns(): array
+	{
+		return [
+
+			//TextColumn::make('latitude'),
+			//TextColumn::make('longitude'),
+			MapColumn::make('location')
+				->extraImgAttributes(
+					fn($record): array => ['title' => $record->latitude . ',' . $record->longitude]
+				)
+				->height('150')
+				->width('150')
+				->type('hybrid')
+				->zoom(16),
+			TextColumn::make('demand_code'),
+			TextColumn::make('area_id')
+				->label(__('resources.categories.area'))
+				->formatStateUsing(fn($state): string => Area::find($state)?->name ?? 'N/A')
+				->sortable(),
+		];
+	}
+
+	protected function getTableFilters(): array
+	{
+		return [
+			RadiusFilter::make('location')
+				->section('Radius Filter')
+				->selectUnit(),
+			MapIsFilter::make('map'),
+		];
+	}
+
+	protected function getTableActions(): array
+	{
+		return [
+			//Tables\Actions\ViewAction::make(),
+			//Tables\Actions\EditAction::make(),
+			Tables\Actions\ViewAction::make()
+				->url(fn($record) => ViewDemand::getUrl(['record' => $record])),
+			Tables\Actions\EditAction::make()
+				->url(fn($record) => EditDemand::getUrl(['record' => $record])),
+			GoToAction::make()
+				->zoom(17)
+				->label('Ver no mapa'),
+			//RadiusAction::make(),
+		];
+	}
+
+	protected function getData(): array
+	{
+		$locations = $this->getRecords();
+
+		$data = [];
+
+		foreach ($locations as $location) {
+			$data[] = [
+				'location' => [
+					'lat' => $location->latitude ? round(floatval($location->latitude), static::$precision) : 0,
+					'lng' => $location->longitude ? round(floatval($location->longitude), static::$precision) : 0,
+				],
+				'id'      => $location->id,
+			];
+		}
+
+		return $data;
+	}
+}

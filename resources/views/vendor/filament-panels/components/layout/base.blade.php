71f56dc9039b9f7bca26b36db8551396dd1b9967
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-04-08 10:45:22 -0300

    -

--- resources/views/vendor/filament-panels/components/layout/base.blade.php
+++ resources/views/vendor/filament-panels/components/layout/base.blade.php
@@ -16,7 +16,7 @@
 
         <meta charset="utf-8" />
         <meta name="csrf-token" content="{{ csrf_token() }}" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <!--<meta name="viewport" content="width=device-width, initial-scale=1" />-->
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
 
 

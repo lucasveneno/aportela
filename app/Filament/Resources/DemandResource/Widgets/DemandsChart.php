959b56aceac3e057ae693e3d4429398432e6f15d
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-04-07 23:40:52 -0300

    -

--- app/Filament/Resources/DemandResource/Widgets/DemandsChart.php
+++ app/Filament/Resources/DemandResource/Widgets/DemandsChart.php
@@ -14,7 +14,7 @@ class DemandsChart extends ChartWidget
 
     protected static ?string $maxHeight = '300px';
 
-    protected int|string|array $columnSpan = 'lg';
+    protected int|string|array $columnSpan = 'full';
 
     public ?string $filter = 'month';
 

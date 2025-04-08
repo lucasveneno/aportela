5e8231479301f1842b04b76e16dee7cc704eb724
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-04-08 00:20:59 -0300

    -

--- app/Filament/Resources/DemandResource/Widgets/StatsOverview.php
+++ app/Filament/Resources/DemandResource/Widgets/StatsOverview.php
@@ -24,48 +24,43 @@ class StatsOverview extends BaseWidget
             now()->startOfMonth(),
             now()->endOfMonth()
         ]);
-        
+
         // Non-admin users
         if (!$isAdmin) {
 
             $baseQuery->where('user_id', $user->id);
-            $monthlyQuery->where('user_id', $user->id);
 
             // non-admin stats
             $totalDemands = $baseQuery->count();
-            $monthlyDemands = $monthlyQuery->count();
             $newDemands = $baseQuery->clone()->whereDate('created_at', today())->count();
             $pendingDemands = $baseQuery->clone()->where('draft', '1')->count();
             $completedDemands = $baseQuery->clone()->where('draft', '0')->count();
 
             return [
 
-                // non-admin users
                 Stat::make(__('resources.widgets.stats_overview.new_demands'), $newDemands)
                     ->description(__('resources.widgets.stats_overview.new_demands_description'))
                     ->descriptionIcon('heroicon-o-arrow-trending-up')
                     ->color('info') //(primary, success, warning, danger, info)
                     ->chart([7, 3, 4, 5, 6, 3, 5]),
-                // non-admin users
+
                 Stat::make(__('resources.widgets.stats_overview.total_demands'), $totalDemands)
                     ->description(__('resources.widgets.stats_overview.total_demands_description'))
                     ->descriptionIcon('heroicon-o-document-text')
                     ->chart([7, 3, 4, 5, 6, 3, 5])
                     ->color('warning'),
-                // non-admin users
+
                 Stat::make(__('resources.widgets.stats_overview.pending_demands'), $pendingDemands)
                     ->description(__('resources.widgets.stats_overview.pending_demands_description'))
                     ->descriptionIcon('heroicon-o-clock')
                     ->chart([3, 5, 2, 4, 6, 3, 2])
                     ->color('danger'),
-                // non-admin users
+
                 Stat::make(__('resources.widgets.stats_overview.completed_demands'), $completedDemands . ' / 100')
                     ->description(__('resources.widgets.stats_overview.completed_demands_description'))
                     ->descriptionIcon('heroicon-o-check-circle')
                     ->chart([2, 4, 3, 1, 5, 6, 7])
                     ->color('success'),
-
-
 
                 // Only show to admins
                 /*$isAdmin ? Stat::make('Active Users', User::where('is_active', true)->count())

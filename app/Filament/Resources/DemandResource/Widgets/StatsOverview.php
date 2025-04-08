4e7c36887169c795a20c97a9d4e31f779fe1dcaa
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-04-08 00:28:25 -0300

    -

--- app/Filament/Resources/DemandResource/Widgets/StatsOverview.php
+++ app/Filament/Resources/DemandResource/Widgets/StatsOverview.php
@@ -46,7 +46,7 @@ class StatsOverview extends BaseWidget
 
                 Stat::make(__('resources.widgets.stats_overview.total_demands'), $totalDemands)
                     ->description(__('resources.widgets.stats_overview.total_demands_description'))
-                    ->descriptionIcon('heroicon-o-document-text')
+                    ->descriptionIcon('clipboard-document-check')
                     ->chart([7, 3, 4, 5, 6, 3, 5])
                     ->color('warning'),
 

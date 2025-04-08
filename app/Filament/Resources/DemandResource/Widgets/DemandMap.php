4d1dacee3a24e193ee6d8f1c5057202e1fad685c
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-04-08 13:07:57 -0300

    -

--- app/Filament/Resources/DemandResource/Widgets/DemandMap.php
+++ app/Filament/Resources/DemandResource/Widgets/DemandMap.php
@@ -27,31 +27,30 @@ class DemandMap extends MapWidget
 
     protected function getData(): array
     {
-    	/**
-    	 * You can use whatever query you want here, as long as it produces a set of records with your
-    	 * lat and lng fields in them.
-    	 */
+        /**
+         * You can use whatever query you want here, as long as it produces a set of records with your
+         * lat and lng fields in them.
+         */
         //$locations = \App\Models\Demand::all()->limit(500);
 
         $locations = Demand::query();
 
-		if (!auth()->user()->isAdmin()) {
-			$locations->where('user_id', auth()->id());
-		}
+        if (!auth()->user()->isAdmin()) {
+            $locations->where('user_id', auth()->id());
+        }
 
 
         $data = [];
 
-        foreach ($locations as $location)
-        {
-			/**
-			 * Each element in the returned data must be an array
-			 * containing a 'location' array of 'lat' and 'lng',
-			 * and a 'label' string (optional but recommended by Google
-			 * for accessibility.
-			 *
-			 * You should also include an 'id' attribute for internal use by this plugin
-			 */
+        foreach ($locations as $location) {
+            /**
+             * Each element in the returned data must be an array
+             * containing a 'location' array of 'lat' and 'lng',
+             * and a 'label' string (optional but recommended by Google
+             * for accessibility.
+             *
+             * You should also include an 'id' attribute for internal use by this plugin
+             */
             $data[] = [
                 'location'  => [
                     'lat' => $location->latitude ? round(floatval($location->latitude), static::$precision) : 0,
@@ -59,19 +58,19 @@ class DemandMap extends MapWidget
                 ],
 
                 'label'     => $location->latitude . ',' . $location->longitude,
-                
+
                 'id' => $location->getKey(),
 
-				/**
-				 * Optionally you can provide custom icons for the map markers,
-				 * either as scalable SVG's, or PNG, which doesn't support scaling.
-				 * If you don't provide icons, the map will use the standard Google marker pin.
-				 */
-				//'icon' => [
-				//	'url' => url('images/dealership.svg'),
-				//	'type' => 'svg',
-				//	'scale' => [35,35],
-				//],
+                /**
+                 * Optionally you can provide custom icons for the map markers,
+                 * either as scalable SVG's, or PNG, which doesn't support scaling.
+                 * If you don't provide icons, the map will use the standard Google marker pin.
+                 */
+                //'icon' => [
+                //    'url' => url('images/dealership.svg'),
+                //    'type' => 'svg',
+                //    'scale' => [35, 35],
+                //],
             ];
         }
 

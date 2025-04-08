19e56140f2bd3839ddb7ff1db38343d6ac040358
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-04-08 15:19:13 -0300

    -

--- app/Filament/Resources/DemandResource/Widgets/DemandMap.php
+++ app/Filament/Resources/DemandResource/Widgets/DemandMap.php
@@ -63,8 +63,7 @@ class DemandMap extends MapWidget
                     'lng' => $location->longitude ? round(floatval($location->longitude), static::$precision) : 0,
                 ],
 
-                //'label'     => 'Código: '.$location->demand_code.' '. $location->latitude . ',' . $location->longitude,
-
+                'label'     => 'Código: '.$location->demand_code.' '. $location->latitude . ',' . $location->longitude,
 
                 'id' => $location->getKey(),
 
@@ -78,21 +77,6 @@ class DemandMap extends MapWidget
                 //    'type' => 'svg',
                 //    'scale' => [35, 35],
                 //],
-
-                'icon'     => view(
-                    'filament-panels::widgets.map-label',
-                    [
-                        'dealershipId'   => $location->demand_code,
-                        'dealershipName' => $location->status,
-                        'dealershipIcon' => '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-</svg>
-',
-                    ]
-                )->render(),
-
-
 
             ];
         }

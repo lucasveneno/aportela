ea7a5abeae1769d65c86247f823d3439642b8ca9
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2025-04-07 23:45:51 -0300

    -

--- app/Filament/Resources/DemandResource/Widgets/StatsOverview.php
+++ app/Filament/Resources/DemandResource/Widgets/StatsOverview.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Auth;
 
 class StatsOverview extends BaseWidget
 {
-    protected static ?string $heading = 'Demand Distribution by User';
-
     protected function getStats(): array
     {
         $user = Auth::user();
